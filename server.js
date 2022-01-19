8b48e0a99c3b9cff16132bf9afa8ccc9d093685d
Franco Olivera <francoleonardoolivera@hotmail.com>
2022-01-19 10:41:59 -0300

    add login POST

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const mockUserData=[
     { name:'Mark' },
     { name:'Jill' }
-]
+];
 
 app.get('/users', function(req, res){
     res.json({
@@ -12,7 +15,7 @@ app.get('/users', function(req, res){
         message: 'successfully got users. Nice!',
         users: mockUserData
     })
-})
+});
 
 app.get('/users/:id',function(req,res){
 	console.log(req.params.id)
@@ -21,7 +24,28 @@ app.get('/users/:id',function(req,res){
 		message: 'got one user',
 		user: req.params.id
 	})
-})
+});
+
+app.post('/login', function(req, res){
+    const user = req.body.username;
+    const pass = req.body.password;
+
+    const TestUs = "billy"
+    const TestPass = "1234"
+
+    if (user === TestUs && pass === TestPass){
+        res.json({
+            success: true,
+            message: 'password and username matched :D',
+            token: 'Token place holder'
+        })
+    }else{
+        res.json({
+            success: false,
+            message: 'password and username dont matched D:'
+        })
+    }
+});
 
 app.listen(8000, function(){
     console.log("Server Is Running!");
